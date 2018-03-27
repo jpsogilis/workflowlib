1e3ebff0471cbf36a5351a6f76080490827a7924
Jp Geslin <jean-pierre@sogilis.com>
2018-03-27 04:19:30 +0200

    aq

--- vars/dockerExec.groovy
+++ vars/dockerExec.groovy
@@ -1,4 +1,7 @@
 def call(Map config) {
+  stage('Launch Ganache') {
+    dock.exec(config.name, dockerCmd: 'ganache-cli &')
+  }
   stage(config.title1) {
     dock.exec(config.name, config.dockerCmd1)
   }
